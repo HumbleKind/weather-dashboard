32b0bfda11435d53d43f1ca9399a0f2a093da494
Christian Sinai <christian_sinai@me.com>
2020-08-22 23:44:22 -0700

    updated submit button

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// API key: 87f01c7c0ae95c2907ec5b879ab8afaa
+var citySearch = $("#city-search");
+
+citySearch.on("submit", function(event) {
+    event.preventDefault();
+    var city = $("#city-name").val();
+    console.log(city);
+
+    getWeather(city);
+});
+
+function getWeather(cityName) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=87f01c7c0ae95c2907ec5b879ab8afaa";
+    console.log(queryURL);
+
+    $.ajax({ // WHY AM I GETTING AN ERROR HERE ...
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.main.temp);
+        
+    });
+}
+
+// ... WHEN THE FOLLOWING WORKS?!?!
+var cityName = "Atlanta";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=87f01c7c0ae95c2907ec5b879ab8afaa";
+console.log(queryURL);
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response.main.temp);
+    
+});
