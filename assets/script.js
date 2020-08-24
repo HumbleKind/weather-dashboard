290770f1ea74030d30c57922859cb932d8f74647
Christian Sinai <christian_sinai@me.com>
2020-08-24 16:21:33 -0700

    added temp 5-day forecast card

--- assets/script.js
+++ assets/script.js
@@ -14,32 +14,6 @@ citySearch.on("submit", function(event) {
     $("#search-history").prepend($("<li>").addClass("list-group-item").text(cityName));
 });
 
-// function getWeather(cityName) {
-
-//     // API key: 87f01c7c0ae95c2907ec5b879ab8afaa
-//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=87f01c7c0ae95c2907ec5b879ab8afaa";
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-
-//         // current date in mm/dd/yyyy format
-//         var currentDate = (new Date()).toLocaleDateString('en-US');
-
-//         // current weather icon
-//         var weatherIcon = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png";
-
-//         // search results - city name, date, weather icon
-//         $(".card-title").text(response.name + " " + "(" + currentDate + ")").append("<img src=" + weatherIcon + " />");
-
-//         // search results - temperature, humidity, wind speed, UV index
-//         $("#temperature").text("Temperature: " + (((response.main.temp - 273.15) * 1.8) + 32).toFixed(1) + " ℉");
-//         $("#humidity").text("Humidity: " + response.main.humidity + "%");
-//         $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
-//     });
-// };
-
 function getWeather(cityName) {
 
     // API key: 87f01c7c0ae95c2907ec5b879ab8afaa
@@ -65,14 +39,21 @@ function getWeather(cityName) {
         var weatherIcon = "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + ".png";
 
         // search results - city name, date, weather icon
-        $(".card-title").text(cityName + " " + "(" + currentDate + ")").append("<img src=" + weatherIcon + " />");
+        $("#card-title-city").text(cityName + " " + "(" + currentDate + ")").append("<img src=" + weatherIcon + " />");
 
-        // search results - temperature, humidity, wind speed, UV index
+        // current weather results - temperature, humidity, wind speed, UV index
+        $("#current-temperature").text("Temperature: " + (((response.current.temp - 273.15) * 1.8) + 32).toFixed(1) + " ℉");
+        $("#current-humidity").text("Humidity: " + response.current.humidity + "%");
+        $("#current-wind-speed").text("Wind Speed: " + response.current.wind_speed + " MPH");
+        $("#current-uv-index").text("UV Index: " + response.current.uvi);
+        
+        // 5-day forecast results - date, weather icon, temperature, humidity
+        $("#card-title-date").text(currentDate).append("<img src=" + weatherIcon + " />");
         $("#temperature").text("Temperature: " + (((response.current.temp - 273.15) * 1.8) + 32).toFixed(1) + " ℉");
         $("#humidity").text("Humidity: " + response.current.humidity + "%");
-        $("#wind-speed").text("Wind Speed: " + response.current.wind_speed + " MPH");
-        $("#uv-index").text("UV Index: " + response.current.uvi);
+
         });
+
     });
 
 };
